49003d57024055e16e1353657a1f7d3d88364676
Krithiga Thachanamoorthy <krithigamoorthy@gmail.com>
2026-09-21 19:53:33 +0530

    Added some routing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,31 +36,31 @@ export function HomePage () {
                 </div>
             </div>
 
-            <div classNameName="home-page">
-                <div classNameName="products-grid">
-                <div classNameName="product-container">
-                    <div classNameName="product-image-container">
-                    <img classNameName="product-image"
+            <div className="home-page">
+                <div className="products-grid">
+                <div className="product-container">
+                    <div className="product-image-container">
+                    <img className="product-image"
                         src="images/products/athletic-cotton-socks-6-pairs.jpg" />
                     </div>
 
-                    <div classNameName="product-name limit-text-to-2-lines">
+                    <div className="product-name limit-text-to-2-lines">
                     Black and Gray Athletic Cotton Socks - 6 Pairs
                     </div>
 
-                    <div classNameName="product-rating-container">
-                    <img classNameName="product-rating-stars"
+                    <div className="product-rating-container">
+                    <img className="product-rating-stars"
                         src="images/ratings/rating-45.png" />
-                    <div classNameName="product-rating-count link-primary">
+                    <div className="product-rating-count link-primary">
                         87
                     </div>
                     </div>
 
-                    <div classNameName="product-price">
+                    <div className="product-price">
                     $10.90
                     </div>
 
-                    <div classNameName="product-quantity-container">
+                    <div className="product-quantity-container">
                     <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -75,41 +75,41 @@ export function HomePage () {
                     </select>
                     </div>
 
-                    <div classNameName="product-spacer"></div>
+                    <div className="product-spacer"></div>
 
-                    <div classNameName="added-to-cart">
+                    <div className="added-to-cart">
                     <img src="images/icons/checkmark.png" />
                     Added
                     </div>
 
-                    <button classNameName="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary">
                     Add to Cart
                     </button>
                 </div>
 
-                <div classNameName="product-container">
-                    <div classNameName="product-image-container">
-                    <img classNameName="product-image"
+                <div className="product-container">
+                    <div className="product-image-container">
+                    <img className="product-image"
                         src="images/products/intermediate-composite-basketball.jpg" />
                     </div>
 
-                    <div classNameName="product-name limit-text-to-2-lines">
+                    <div className="product-name limit-text-to-2-lines">
                     Intermediate Size Basketball
                     </div>
 
-                    <div classNameName="product-rating-container">
-                    <img classNameName="product-rating-stars"
+                    <div className="product-rating-container">
+                    <img className="product-rating-stars"
                         src="images/ratings/rating-40.png" />
-                    <div classNameName="product-rating-count link-primary">
+                    <div className="product-rating-count link-primary">
                         127
                     </div>
                     </div>
 
-                    <div classNameName="product-price">
+                    <div className="product-price">
                     $20.95
                     </div>
 
-                    <div classNameName="product-quantity-container">
+                    <div className="product-quantity-container">
                     <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -124,41 +124,41 @@ export function HomePage () {
                     </select>
                     </div>
 
-                    <div classNameName="product-spacer"></div>
+                    <div className="product-spacer"></div>
 
-                    <div classNameName="added-to-cart">
+                    <div className="added-to-cart">
                     <img src="images/icons/checkmark.png" />
                     Added
                     </div>
 
-                    <button classNameName="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary">
                     Add to Cart
                     </button>
                 </div>
 
-                <div classNameName="product-container">
-                    <div classNameName="product-image-container">
-                    <img classNameName="product-image"
+                <div className="product-container">
+                    <div className="product-image-container">
+                    <img className="product-image"
                         src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
                     </div>
 
-                    <div classNameName="product-name limit-text-to-2-lines">
+                    <div className="product-name limit-text-to-2-lines">
                     Adults Plain Cotton T-Shirt - 2 Pack
                     </div>
 
-                    <div classNameName="product-rating-container">
-                    <img classNameName="product-rating-stars"
+                    <div className="product-rating-container">
+                    <img className="product-rating-stars"
                         src="images/ratings/rating-45.png" />
-                    <div classNameName="product-rating-count link-primary">
+                    <div className="product-rating-count link-primary">
                         56
                     </div>
                     </div>
 
-                    <div classNameName="product-price">
+                    <div className="product-price">
                     $7.99
                     </div>
 
-                    <div classNameName="product-quantity-container">
+                    <div className="product-quantity-container">
                     <select>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -173,14 +173,14 @@ export function HomePage () {
                     </select>
                     </div>
 
-                    <div classNameName="product-spacer"></div>
+                    <div className="product-spacer"></div>
 
-                    <div classNameName="added-to-cart">
+                    <div className="added-to-cart">
                     <img src="images/icons/checkmark.png" />
                     Added
                     </div>
 
-                    <button classNameName="add-to-cart-button button-primary">
+                    <button className="add-to-cart-button button-primary">
                     Add to Cart
                     </button>
                 </div>
